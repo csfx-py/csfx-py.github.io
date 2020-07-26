837f55a0c94b05a55234bea82a48cea422489bc2
Shreyas Prabhu <59968063+csfx-py@users.noreply.github.com>
2020-07-27 00:15:15 +0530

    Add files via upload
    initial commit

---
+++ apps.js
@@ -0,0 +1,27 @@
+const txtSet = ["Front-end Developer", "Back-end Developer", "Android Developer"];
+let count = 0;
+let index = 0;
+let currTxt = "";
+let letter = "";
+
+// particle js config loader
+particlesJS.load("particle-class", "assets/particlesjs-config.json", function () {
+  console.log("callback - particles.js config loaded");
+});
+
+// typing animation script
+(function type() {
+  if (count === txtSet.length) {
+    count = 0;
+  }
+  currTxt = txtSet[count];
+  letter = currTxt.slice(0, ++index);
+
+  document.querySelector(".typing").textContent = letter;
+  if (letter.length === currTxt.length) {
+    count++;
+    index = 0;
+  }
+
+  setTimeout(type, 300);
+})();
